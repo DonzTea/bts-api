79df7ae858d5eccdfe79215e5e8528b598ae1ae1
Doni Rahma Tiana <donirahmatiana@gmail.com>
2021-05-07 17:24:52 +0700

    add comment on db/sequelize.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bts', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  'bts', // db name
+  'root', // username
+  'root', // password
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+);
 
 module.exports = sequelize;
